35f267a5562c46d9a4f8433cc2cfda66332bce42
Jeff Verkoeyen <featherless@google.com>
2017-10-03 18:00:31 -0400

    Trying out xcbuild.

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -5,17 +5,12 @@ set -e
 # Display commands to stderr.
 set -x
 
-gem install cocoapods --no-rdoc --no-ri --no-document --quiet
-gem install xcpretty --no-rdoc --no-ri --no-document --quiet
+brew install cmake
 
-# Test pull request.
+git clone https://github.com/facebook/xcbuild
+cd xcbuild
+git submodule update --init
+make
 
-sudo xcode-select --switch /Applications/Xcode_8.2.app/Contents/Developer
-xcodebuild -version
-
-cd github/motion-transitions-objc
-pod install --repo-update
-
-xcodebuild build -workspace MotionTransitions.xcworkspace -scheme Catalog -sdk "iphonesimulator" -destination "name=iPhone 6s,OS=10.0" ONLY_ACTIVE_ARCH=YES | xcpretty && exit ${PIPESTATUS[0]}
-
-bash <(curl -s https://codecov.io/bash)
+cd ../github/motion-transitions-objc
+../../xcbuild/build/xcbuild -workspace MotionTransitions.xcworkspace -scheme Catalog -sdk "iphonesimulator" -destination "name=iPhone 6s,OS=10.0" ONLY_ACTIVE_ARCH=YES | xcpretty && exit ${PIPESTATUS[0]}
