65ac17e7e0f8907ae1851357fd812840d1a4c629
Jeff Verkoeyen <featherless@google.com>
2017-10-03 14:57:53 -0400

    Update build script.

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -6,13 +6,11 @@ set -e
 set -x
 
 gem install cocoapods --no-rdoc --no-ri --no-document --quiet
-git clone https://github.com/phacility/arcanist.git
-git clone https://github.com/phacility/libphutil.git
-git clone --recursive https://github.com/material-foundation/material-arc-tools.git
+gem install xcpretty --no-rdoc --no-ri --no-document --quiet
 
 cd github/motion-transitions-objc
 pod install --repo-update
 
-xcodebuild build -workspace MotionTransitions.xcworkspace -scheme Catalog -sdk "iphonesimulator10.1" -destination "name=iPhone 6s,OS=10.1" ONLY_ACTIVE_ARCH=YES | xcpretty -c;
+xcodebuild build -workspace MotionTransitions.xcworkspace -scheme Catalog -sdk "iphonesimulator" -destination "name=iPhone 6s,OS=10.0" ONLY_ACTIVE_ARCH=YES
 
 bash <(curl -s https://codecov.io/bash)
